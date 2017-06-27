b045d30351b7a71e50a4ea4e9ef3ffb60fb56343
cjhaas <chris@vendiadvertising.com>
2017-06-27 14:34:31 -0500

    Phar doesn't like bash invoation

---
+++ app.php
@@ -0,0 +1,16 @@
+<?php
+
+define( 'VENDI_CLI_FILE', __FILE__ );
+define( 'VENDI_CLI_PATH', dirname( __FILE__ ) );
+
+require VENDI_CLI_PATH . '/includes/autoload.php';
+
+use Vendi\CLI;
+use Symfony\Component\Console\Application;
+
+$create_site_command = new Vendi\CLI\CreateSiteCommand();
+
+$application = new Application( 'Vendi Admin CLI', '0.1-dev' );
+$application->add( $create_site_command );
+$application->setDefaultCommand( $create_site_command->getName() );
+$application->run();
