d6938844f0b6a2122897d7ff524de9796bc0959f
cjhaas <chris@vendiadvertising.com>
2017-06-27 17:42:00 -0500

    Add missing use statement

--- src/Commands/create_site_wizard.php
+++ src/Commands/create_site_wizard.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Input\ArrayInput;
+use Vendi\CLI\site_info;
 
 class create_site_wizard extends Command
 {
