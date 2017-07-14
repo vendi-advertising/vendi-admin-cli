faaf879b1e0b2e1131c5bd2fbbf5a5a5aa179651
cjhaas <chris@vendiadvertising.com>
2017-07-14 16:08:47 -0500

    Remove Sentry. Start self update

--- app.php
+++ app.php
@@ -5,12 +5,6 @@ define( 'VENDI_CLI_PATH', dirname( __FILE__ ) );
 
 require VENDI_CLI_PATH . '/includes/autoload.php';
 
-// $client = new Raven_Client('https://d3e0fec781a84dc28a59ad61d05c685a:d1d0b54ff2804a4e91ec42fa07638548@sentry.io/186243');
-// $error_handler = new Raven_ErrorHandler( $client );
-// $error_handler->registerExceptionHandler();
-// $error_handler->registerErrorHandler();
-// $error_handler->registerShutdownFunction();
-
 $create_site_wizard = new Vendi\CLI\Commands\create_site_wizard();
 
 $application = new Symfony\Component\Console\Application( 'Vendi Admin CLI', '0.1-dev' );
@@ -29,5 +23,3 @@ $application->add( new Vendi\CLI\Commands\libsodium_installer() );
 
 // $application->setDefaultCommand( $create_site_wizard->getName() );
 $application->run();
-
-
